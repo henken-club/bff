ecadd0fb1f79718fa41019b84182396ccc35ce0e
SnO2WMaN <me@sno2wman.net>
2021-10-02 16:41:35 +0900

    env.d.ts

---
+++ src/types/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv extends NodeJS.ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+
+    readonly SERVICE_URL_MAIN: string;
+  }
+}
