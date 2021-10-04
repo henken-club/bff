ad4073048cb7aef49cc207aeb3e03eb50777a018
SnO2WMaN <me@sno2wman.net>
2021-10-04 17:24:25 +0900

    No nest

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
 
+    readonly PORT: string;
     readonly SERVICE_URL_MAIN: string;
   }
 }
