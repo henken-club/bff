901b488efeece8ff8cd6e3fe1a78e806067d520d
SnO2WMaN <me@sno2wman.net>
2021-10-07 02:03:26 +0900

    一旦userIdを返却するように実装

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,26 @@
-export const verifyToken = async (token: string): Promise<{userId: string}> => {
-  return {userId: 'userId'};
+import {promisify} from 'util';
+
+import {decode, verify, JsonWebTokenError} from 'jsonwebtoken';
+import createJwksClient from 'jwks-rsa';
+
+const jwksClient = createJwksClient({jwksUri: process.env.JWKS_URI});
+
+export const verifyToken = async (
+  token: string,
+): Promise<{userId: string | null}> => {
+  const decoded = decode(token, {complete: true});
+  if (!decoded?.header.kid) throw new JsonWebTokenError('Invalid token');
+
+  const getSigningKey = promisify(jwksClient.getSigningKey);
+  const publicKey = await (
+    await getSigningKey(decoded.header.kid)
+  ).getPublicKey();
+
+  const tokenData = verify(token, publicKey, {
+    audience: process.env.AUDIENCE,
+    issuer: process.env.ISSUER,
+  });
+  if (typeof tokenData === 'string') throw new Error('Invalid token data');
+
+  return {userId: tokenData.sub || null};
 };
